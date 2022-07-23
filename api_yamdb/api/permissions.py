ecc01319b681c64101927bcaf3fbad046562b636
Александр Фокин <wegnagun@bk.ru>
2022-07-23 17:43:00 +0300

    pytest done!

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,11 +2,10 @@ from rest_framework import permissions
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
-    class IsAdminOrReadOnly(permissions.BasePermission):
-        def has_permission(self, request, view):
-            if request.method in permissions.SAFE_METHODS:
-                return True
-            return request.user.is_authenticated and request.user.is_admin
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_authenticated
+                    and request.user.is_admin))
 
 
 class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
